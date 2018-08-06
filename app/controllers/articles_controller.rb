dfe98b4a701b1fcfc7ea2af17542bc28968fbc00
nctl144 <nctl144@gmail.com>
2018-08-05 23:39:58 -0500

    print error if there's any

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,19 +2,23 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def show
     @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   private
