741c35c3f5765bfab8ea6f9eff7ee3a9da6d9e3f
nctl144 <nctl144@gmail.com>
2018-08-19 13:16:43 -0400

    add comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_06_041010) do
+ActiveRecord::Schema.define(version: 2018_08_19_162334) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,4 +22,14 @@ ActiveRecord::Schema.define(version: 2018_08_06_041010) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
 end
