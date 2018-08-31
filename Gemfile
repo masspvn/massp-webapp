29b4e8de62fac15cc462b3afdd17952d4501d406
nctl144 <nctl144@gmail.com>
2018-08-31 16:43:34 -0400

    add password auth

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '~> 2.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
+gem 'bcrypt', '3.1.12'
 gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_31_201905) do
+ActiveRecord::Schema.define(version: 2018_08_31_203947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2018_08_31_201905) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
